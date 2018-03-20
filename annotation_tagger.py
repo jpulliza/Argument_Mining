b2ba8a4e22baf1893491a0a81d9c8ac7d4daac32
Jonathan Pulliza <jpulliza@gmail.com>
2018-03-20 14:04:55 -0400

    add tags to clean text on a character basis and output html file

--- annotation_tagger.py
+++ annotation_tagger.py
@@ -151,12 +151,20 @@ def add_callout_ids(db, Word, underlying_file, annotators):
     error_file.writelines(errors)
 
 
+def return_annotation_positions(annotation, text):
+    if text.count(annotation) == 1:
+        start_index = text.index(annotation)
+        end_index = start_index + len(annotation)
+        return start_index, end_index
+    else:
+        return "ERROR", "ERROR"
+
+
 def clean_text_input(underlying_file):
     original_text_file = data_path + "/original/" + underlying_file + ".orig.txt"
-    df = pd.read_csv(data_path + "/expert_annotated/" + underlying_file + '.ea.txt', sep='\t', error_bad_lines=False)
     original_text = open(original_text_file, encoding="utf8").read()
     clean_text = ' '.join(repr(original_text).replace('\\n\\n', ' <p> ').replace('\\n', ' ').split(sep=' '))
-    return  clean_text
+    return clean_text
 
 
 def append_word_doc_index(source, text, word_index):
@@ -206,3 +214,66 @@ def make_doc_index(doc_index):
         append_doc_index(file, doc_index)
 
 
+def append_tag_dictionary(tag_dictionary, start_position, end_position, annotator, annotation_id, class_type):
+
+    start_tag = "<annotator_{0} id={1} class='{2}'>".format(annotator, annotation_id, class_type)
+
+    if start_position in tag_dictionary:
+        if start_tag not in tag_dictionary[start_position]:
+            new_tags = tag_dictionary[start_position] + start_tag
+            tag_dictionary[start_position] = new_tags
+    else:
+        tag_dictionary[start_position] = start_tag
+
+    end_tag = "</annotator_{0}>".format(annotator)
+
+    if end_position in tag_dictionary:
+        if end_tag not in tag_dictionary[end_position]:
+            new_tags = tag_dictionary[end_position] + end_tag
+            tag_dictionary[end_position] = new_tags
+    else:
+        tag_dictionary[end_position] = end_tag
+
+
+def add_annotation_tags(underlying_file):
+    tags = {}
+    clean_text = clean_text_input(underlying_file)
+    df = pd.read_csv(data_path + "/expert_annotated/" + underlying_file + '.ea.txt', sep='\t', error_bad_lines=False)
+    for row in df.iterrows():
+        annotator_id = str(row[1]['Annotator Name'])
+
+        callout_text = str(row[1]['Calling-out Spanned Text'])
+        callout_id = str(row[1]['Knowtator Calling-out ID'])
+
+        callout_start, callout_end = return_annotation_positions(callout_text, clean_text)
+
+        append_tag_dictionary(tags, callout_start, callout_end, annotator_id, callout_id, "callout")
+
+        target_text = str(row[1]['Target Spanned Text'])
+        target_id = str(row[1]['Knowtator Target ID'])
+
+        target_start, target_end = return_annotation_positions(target_text, clean_text)
+
+        append_tag_dictionary(tags, target_start, target_end, annotator_id, target_id, "target")
+
+    clean_text_list = list(clean_text)
+
+    for key in tags:
+        try:
+            clean_text_list[key] = '{0}{1}'.format(tags[key], clean_text[key])
+        except:
+            continue
+
+    tagged_text = ''.join(clean_text_list)
+
+    return tagged_text
+
+
+for underlying_file in underlying_files:
+    output_file = data_path + "/output/" + underlying_file + '_output_overlap.html'
+    g = codecs.open(output_file, 'w', 'utf-8')
+
+    tagged_text = add_annotation_tags(underlying_file)
+
+    g.write('<!DOCTYPE html><html><head><link rel="stylesheet" href="styles.css"></head><body>'
+            + tagged_text + '</body></html>')
